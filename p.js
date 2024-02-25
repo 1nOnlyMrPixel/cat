a46312ac9e523c79975a0458fb5a2a704d4529f4
R4ven <160066890+1nOnlyMrPixel@users.noreply.github.com>
2024-02-25 14:49:50 +0530

    Done

---
+++ p.js
@@ -0,0 +1,12 @@
+// document.getElementById("title").style.visibility="";
+document.getElementById("yesbtn").onclick=function()
+{
+    // alert("Clicked");
+    document.getElementById("title").style.visibility="hidden";
+    document.getElementById("imbox").src="./linus.gif";
+}
+document.getElementById("nobtn").onclick=function()
+{
+   document.getElementById("title").style.visibility="hidden";
+    document.getElementById("imbox").src="./linus.gif";
+}
