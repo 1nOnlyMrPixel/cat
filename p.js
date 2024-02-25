355ec83fbc8f2d874b675573c32d3b37d1979ba0
R4ven <160066890+1nOnlyMrPixel@users.noreply.github.com>
2024-02-25 15:01:23 +0530

    New

--- p.js
+++ p.js
@@ -1,12 +1,17 @@
 // document.getElementById("title").style.visibility="";
-document.getElementById("yesbtn").onclick=function()
-{
-    // alert("Clicked");
-    document.getElementById("title").style.visibility="hidden";
-    document.getElementById("imbox").src="./linus.gif";
-}
-document.getElementById("nobtn").onclick=function()
-{
-   document.getElementById("title").style.visibility="hidden";
-    document.getElementById("imbox").src="./linus.gif";
-}
+document.getElementById("yesbtn").onclick = function () {
+  // alert("Clicked");
+  document.getElementById("title").style.visibility = "hidden";
+  document.getElementById("imbox").src = "./linus.gif";
+  document.getElementById("nobtn").style.backgroundColor = "rgb(233, 233, 237)";
+  document.getElementById("yesbtn").style.backgroundColor =
+    "rgb(168, 255, 168)";
+};
+document.getElementById("nobtn").onclick = function () {
+  document.getElementById("yesbtn").style.backgroundColor =
+    "rgb(233, 233, 237)";
+
+  document.getElementById("nobtn").style.backgroundColor = "rgb(255, 145, 145)";
+  document.getElementById("title").style.visibility = "hidden";
+  document.getElementById("imbox").src = "./linus.gif";
+};
